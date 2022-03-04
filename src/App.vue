968b001fd67ede3c264ddb8a3c81471e560e5e91
takyu <takyu613.study@gmail.com>
2022-03-04 20:05:48 +0900

    Add to store book information to localstorage from App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <Header />
     <v-main>
       <v-container>
-        <router-view />
+        <router-view @add-book-list="addBook" />
       </v-container>
     </v-main>
     <Footer />
@@ -14,12 +14,51 @@
 import Header from "@/global/HeaderPart";
 import Footer from "@/global/FooterPart";
 
+const STORAGE_KEY = "books";
+
 export default {
   name: "App",
 
   components: {
     Header,
     Footer,
+  },
+
+  data() {
+    return {
+      books: [],
+      newBook: "",
+    };
+  },
+  mounted() {
+    if (localStorage.getItem(STORAGE_KEY)) {
+      try {
+        this.books = JSON.parse(localStorage.getItem(STORAGE_KEY));
+      } catch (e) {
+        localStorage.removeItem(STORAGE_KEY);
+        console.error(e);
+      }
+    }
+  },
+  methods: {
+    addBook(e) {
+      this.books.push({
+        id: this.books.length,
+        title: e.title,
+        image: e.image,
+        description: e.description,
+        readData: "",
+        memo: "",
+      });
+      this.saveBooks();
+    },
+    removeBook(n) {
+      this.books.splice(n, 1);
+      this.saveBooks();
+    },
+    saveBooks() {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(this.books));
+    },
   },
 };
 </script>
--- src/pages/BookSearch.vue
+++ src/pages/BookSearch.vue
@@ -81,46 +81,24 @@ export default {
     async search(keyword) {
       this.searchResults = [];
 
-      // Query String
       const baseUrl = "https://www.googleapis.com/books/v1/volumes?";
-
-      // params
       const params = {
         q: `intitle:${keyword}`,
         maxResults: 40,
       };
-
       const queryParams = new URLSearchParams(params);
-
-      // check
-      // console.log(baseUrl + queryParams);
-
-      // get json file with fetch
       const response = await fetch(baseUrl + queryParams).then((response) =>
         response.json()
       );
-
-      // check
-      console.log(response);
 
       if (!response.items) {
         console.log("検索しましたが、見つかりませんでした。");
         return;
       }
 
-      // push array
       for (let book of response.items) {
-        // check
-        // console.log(response.items);
-        // console.log(book);
-
-        // get title
         let title = book.volumeInfo.title;
-
-        // get image links
         let img = book.volumeInfo.imageLinks;
-
-        // get description
         let description = book.volumeInfo.description;
 
         this.searchResults.push({
@@ -130,6 +108,9 @@ export default {
           isShow: false,
         });
       }
+    },
+    addBookList(index) {
+      this.$emit("add-book-list", this.searchResults[index]);
     },
   },
 };
