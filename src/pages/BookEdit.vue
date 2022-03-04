e06352d337f0679a7b4dc52ab7bd701537913fab
takyu <takyu613.study@gmail.com>
2022-03-04 20:48:08 +0900

    Fix to asyncronous process between App.vue and BookEdit.vue

--- src/pages/BookEdit.vue
+++ src/pages/BookEdit.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    {{ books }}
+    {{ book.title }}
   </div>
 </template>
 
@@ -9,6 +9,20 @@ export default {
   name: "BookEdit",
   props: {
     books: Array,
+  },
+  data() {
+    return {
+      book: "",
+    };
+  },
+  beforeRouteEnter(to, from, next) {
+    next((vm) => {
+      // access to component public instance via `vm`
+      vm.$nextTick(() => {
+        vm.book = vm.books[vm.$route.params.id];
+        console.log(vm.book);
+      });
+    });
   },
 };
 </script>
