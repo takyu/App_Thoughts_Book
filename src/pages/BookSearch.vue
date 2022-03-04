66074322580e4f3f24403036f309f110f2af6cb0
takyu <takyu613.study@gmail.com>
2022-03-04 19:33:25 +0900

    Create book search page

--- src/pages/BookSearch.vue
+++ src/pages/BookSearch.vue
@@ -1,3 +1,136 @@
 <template>
-  <div>BookSearch</div>
+  <div>
+    <v-row class="my-2">
+      <v-col cols="12">
+        <v-text-field
+          label="本のタイトルを検索"
+          v-model="keyword"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row class="mb-5">
+      <v-btn
+        class="me-5"
+        rounded="pill"
+        color="success"
+        size="large"
+        @click="search(keyword)"
+      >
+        検索する</v-btn
+      >
+      <v-btn rounded="pill" color="warning" size="large" to="/"
+        >一覧に戻る</v-btn
+      >
+    </v-row>
+    <v-row class="my-5">
+      <v-col
+        cols="12"
+        md="6"
+        v-for="(book, index) in searchResults"
+        :key="book.index"
+      >
+        <v-card class="mx-auto">
+          <v-row>
+            <v-col cols="4">
+              <v-img :src="book.image"></v-img>
+            </v-col>
+            <v-col cols="8">
+              <v-card-title>{{ book.title }}</v-card-title>
+              <v-card-actions>
+                <v-btn
+                  variant="outlined"
+                  icon
+                  color="secondary"
+                  @click="addBookList(index)"
+                  ><v-icon>mdi-format-list-bulleted-square</v-icon>
+                </v-btn>
+
+                <v-btn
+                  :icon="book.isShow ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+                  @click="book.isShow = !book.isShow"
+                ></v-btn>
+              </v-card-actions>
+
+              <v-expand-transition>
+                <div v-show="book.isShow">
+                  <v-divider></v-divider>
+
+                  <v-card-text>
+                    {{ book.description }}
+                  </v-card-text>
+                </div>
+              </v-expand-transition>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "BookSearch",
+  data() {
+    return {
+      keyword: "",
+      searchResults: [],
+    };
+  },
+  methods: {
+    async search(keyword) {
+      this.searchResults = [];
+
+      // Query String
+      const baseUrl = "https://www.googleapis.com/books/v1/volumes?";
+
+      // params
+      const params = {
+        q: `intitle:${keyword}`,
+        maxResults: 40,
+      };
+
+      const queryParams = new URLSearchParams(params);
+
+      // check
+      // console.log(baseUrl + queryParams);
+
+      // get json file with fetch
+      const response = await fetch(baseUrl + queryParams).then((response) =>
+        response.json()
+      );
+
+      // check
+      console.log(response);
+
+      if (!response.items) {
+        console.log("検索しましたが、見つかりませんでした。");
+        return;
+      }
+
+      // push array
+      for (let book of response.items) {
+        // check
+        // console.log(response.items);
+        // console.log(book);
+
+        // get title
+        let title = book.volumeInfo.title;
+
+        // get image links
+        let img = book.volumeInfo.imageLinks;
+
+        // get description
+        let description = book.volumeInfo.description;
+
+        this.searchResults.push({
+          title: title ?? "",
+          image: img ? img.thumbnail : "",
+          description: description ? description.slice(0, 100) : "",
+          isShow: false,
+        });
+      }
+    },
+  },
+};
+</script>
